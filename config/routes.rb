d72165eda912b8721f96df16f98f148a4188d14c
speedmax <subjective@gmail.com>
2010-10-16 15:26:39 +1100

    - Move auth controller into the app  - Add profile edit show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Redcity::Application.routes.draw do
   resources :messages, :except => [:edit, :update]
   resources :profiles
   
+
+  # Account/Auth
+  match '/login'                  => 'auth#new',      :as => :login
+  match '/auth/:service/callback' => 'auth#callback'
+  match '/auth/failure'           => 'auth#failure'
+  match '/logout'                 => 'auth#destroy',  :as => :logout
+
   root :to => 'profiles#index'
 end
