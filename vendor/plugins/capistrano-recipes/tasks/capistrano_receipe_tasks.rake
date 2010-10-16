d986a5290a9843eac4349d9483853019cbc720f2
speedmax <subjective@gmail.com>
2010-10-16 23:20:23 +1100

    remove cap plugin

--- vendor/plugins/capistrano-recipes/tasks/capistrano_receipe_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :capistrano_receipe do
-#   # Task goes here
-# end
--- vendor/plugins/capistrano-recipes/test/capistrano_receipe_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class Capistrano-receipeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
