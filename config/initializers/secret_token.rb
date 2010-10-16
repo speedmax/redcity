71e6925b23286dc09390b5c7a0fd1c2c839f5453
speedmax <subjective@gmail.com>
2010-10-16 11:04:50 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redcity::Application.config.secret_token = 'a33b8538a8aa0c71ca42016b76330cf2fe2a47770696fb0bf49ddb1e2767ef343caab6985ce82d5ea08bc23d604d090c9b978f2cc0d48a9f7663f55fe47cf88a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Redcity::Application.config.session_store :cookie_store, :key => '_redcity_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Redcity::Application.config.session_store :active_record_store
